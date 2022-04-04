58858ac6dbff2b123aa26607c078235471298576
OlenaPustova <i.am.elena.p@gmail.com>
2022-04-04 20:09:45 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,7 +2,6 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryEl = document.querySelector(".gallery");
-// const bodyEl = document.querySelector("body");
 
 const imagesMarkup = createGalleryItem(galleryItems);
 
