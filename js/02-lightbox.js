02c76a80e975b4c861256fa9c00a0f985e8dd179
OlenaPustova <i.am.elena.p@gmail.com>
2022-04-04 16:58:57 +0300

    second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+// const bodyEl = document.querySelector("body");
+
+const imagesMarkup = createGalleryItem(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function createGalleryItem(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
